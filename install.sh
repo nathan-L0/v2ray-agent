f60f5b692a3b17dd04707012372d27f65bffd893
nathan-L0 <59117700+nathan-L0@users.noreply.github.com>
2025-11-25 11:37:16 +0800

    Update author and GitHub link in install.sh

--- install.sh
+++ install.sh
@@ -9932,9 +9932,9 @@ singBoxVersionManageMenu() {
 menu() {
     cd "$HOME" || exit
     echoContent red "\n=============================================================="
-    echoContent green "作者：mack-a"
+    echoContent green "作者：Alin"
     echoContent green "当前版本：v3.4.21"
-    echoContent green "Github：https://github.com/mack-a/v2ray-agent"
+    echoContent green "Github：https://github.com/nathan-L0/v2ray-agent"
     echoContent green "描述：八合一共存脚本\c"
     showInstallStatus
     checkWgetShowProgress
